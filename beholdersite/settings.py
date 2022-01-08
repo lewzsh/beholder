1b9db9c420e3e8fee5c58812a7483d92b6ed37c0
lewis <casibila@gmail.com>
2022-01-07 17:07:02 -0800

    still attempting to resolve domain error

--- beholdersite/settings.py
+++ beholdersite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,beholdermagazine.com").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
